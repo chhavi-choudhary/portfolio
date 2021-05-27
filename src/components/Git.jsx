d468a0c385ca88b83a81ac234e2fba43cf02d3ff
Chhavi <Chhavi.choudhary75@gmail.com>
2021-05-27 23:45:55 +0200

    add projects

--- src/components/Git.jsx
+++ src/components/Git.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Git() {
     return (
         <div>
+            <p>I worked on some projects and used HTML,CSS,Javascript,React</p>
             <p>To know about my project have a look on my GitHub Profile.</p>
             <a href="https://github.com/chhavi-choudhary" >GitHub</a>
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Project() {
     return (
         <div>
-            My projects
+            <p>My projects</p>
+            <h1>Wild Code Bar</h1>
+            <a href="">Wild Code Bar</a>
+            <h1>Explore NASA</h1>
+            <a href="">Explore NASA</a>
         </div>
     )
 }
