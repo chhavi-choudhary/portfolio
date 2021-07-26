de4b9ed28b9bb743339f521cb91f8c2de86cfef8
Chhavi <Chhavi.choudhary75@gmail.com>
2021-07-26 23:43:12 +0200

    create project file basic structure

---
+++ src/components/projec1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function projec1() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/project4.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function project4() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
